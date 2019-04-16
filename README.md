299695775b86694ba5f9395415cbf2daed597cf9
Nadir <nadir.mokkadem@gmail.com>
2019-04-16 13:39:25 +0200

    commit de l'exo 3

---
+++ README.md
@@ -0,0 +1,1 @@
+Repo exo 2
---
+++ exo3.js
@@ -0,0 +1,4 @@
+// prompt = affiche une fenêtre qui invite à entrer un texte et renvoi le texte saisi par l'utilisateur
+var prenom = prompt("Indiquez votre prénom :");
+// littéraux de gabarit permettent d'intégrer des expressions, mettre du texte sur plusieurs lignes
+alert(`Bonjour ${prenom} ! J'espère que vous allez bien.`);
